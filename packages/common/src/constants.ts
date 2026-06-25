fb7ab6584d5fad7da879b7b7c2f24d3de5729c47
labaran1 <adlabaran@gmail.com>
2026-06-25 18:26:43 +0000

    meta data constant

---
+++ packages/common/src/constants.ts
@@ -0,0 +1,10 @@
+export const MODULE_METADATA = {
+  IMPORTS: 'imports',
+  PROVIDERS: 'providers',
+  AGENTS: 'agents',
+  EXPORTS: 'exports',
+};
+
+export const GLOBAL_MODULE_METADATA = 'hadin:module:global';
+export const AGENT_WATERMARK = "hadin:agent";
+export const INJECTABLE_WATERMARK = "hadin:injectable";

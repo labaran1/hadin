7e436d2f97307e05eb65e4eeb47e03106082f961
labaran1 <adlabaran@gmail.com>
2026-06-28 15:34:32 +0000

    exports

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,7 +1,3 @@
 export * from './decorators';
 export * from './enums';
 export * from './interfaces';
-export * from './utils';
-export * from './exceptions';
-export * from './normalizers';
-export * from './services';

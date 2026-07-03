8088238cd43f058b71c82fd5ba8f405354af42c5
labaran1 <adlabaran@gmail.com>
2026-07-03 21:30:27 +0000

    polyfill

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,8 @@
+// TC39 decorator metadata is not available in Node yet, but TypeScript uses
+// this symbol when emitting modern decorators.
+(Symbol as any).metadata ??= Symbol.for('Symbol.metadata');
+
+export * from './constants';
 export * from './decorators';
 export * from './enums';
 export * from './interfaces';

9f11dd7717d976468f045a149a7a495a2a419fd2
labaran1 <adlabaran@gmail.com>
2026-07-05 03:37:05 +0000

    redundant file

--- packages/core/src/errors/unknown-module.exception.ts
+++
@@ -1,6 +0,0 @@
-export class UnknownModuleException extends Error {
-  constructor(name?: string) {
-    super(name ? `Unknown module "${name}".` : 'Unknown module.');
-    this.name = 'UnknownModuleException';
-  }
-}

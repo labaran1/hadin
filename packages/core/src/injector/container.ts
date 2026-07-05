d999d46e71e3c6bd3729c39bed3bc5ccd15a60ec
labaran1 <adlabaran@gmail.com>
2026-07-05 03:20:40 +0000

    container

--- packages/core/src/injector/container.ts
+++ packages/core/src/injector/container.ts
@@ -1,12 +1,10 @@
 import { type Type } from '@hadin/common';
-import { UnknownModuleException } from '../errors';
-import { type ScannedModule } from '../scanner';
-import { getTokenName } from './helpers';
 import { Module } from './module';
 
 export class HadinContainer {
   private readonly modules = new Map<Type, Module>();
   private readonly globalModules = new Set<Module>();
+  private loaded = false;
 
   addModule(type: Type, isGlobal: boolean): Module {
     const existingModule = this.modules.get(type);
@@ -33,46 +31,18 @@ export class HadinContainer {
     return this.modules;
   }
 
-  load(scanned: Map<Type, ScannedModule>): void {
-    for (const scannedModule of scanned.values()) {
-      this.addModule(scannedModule.token, scannedModule.isGlobal);
-    }
-
-    for (const scannedModule of scanned.values()) {
-      const module = this.addModule(scannedModule.token, scannedModule.isGlobal);
-
-      for (const importedType of scannedModule.imports) {
-        const importedModule = this.getModule(importedType);
-
-        if (!importedModule) {
-          throw new UnknownModuleException(getTokenName(importedType));
-        }
-
-        module.addImport(importedModule);
-      }
-
-      for (const provider of scannedModule.providers) {
-        module.addProvider(provider);
-      }
-
-      for (const agent of scannedModule.agents) {
-        module.addAgent(agent);
-      }
-    }
-
-    for (const scannedModule of scanned.values()) {
-      const module = this.addModule(scannedModule.token, scannedModule.isGlobal);
-
-      for (const exportedProvider of scannedModule.exports) {
-        module.addExportedProvider(exportedProvider);
-      }
-    }
-
-    this.bindGlobalScope();
-  }
-
   getGlobalModules(): Set<Module> {
     return this.globalModules;
+  }
+
+  assertNotLoaded(): void {
+    if (this.loaded) {
+      throw new Error('HadinContainer has already been loaded.');
+    }
+  }
+
+  markAsLoaded(): void {
+    this.loaded = true;
   }
 
   bindGlobalScope(): void {

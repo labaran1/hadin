a41cc547162b7940326e4356c2b7e1ce9e7523b8
labaran1 <adlabaran@gmail.com>
2026-07-23 23:33:48 +0000

    exports

--- packages/common/src/interfaces/index.ts
+++ packages/common/src/interfaces/index.ts
@@ -1,5 +1,7 @@
 export * from './agent-options.interface';
 export * from './injectable-options.interface';
 export * from './modules';
+export * from './spawn-metadata.interface';
+export * from './spawn-options.interface';
 export * from './tool-metadata.interface';
 export * from './type.interface';
